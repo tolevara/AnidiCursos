f81cb17c5e96029d2113e68b23be58cc586a82d1
Maria José Toledano <tolevara@gmail.com>
2025-04-03 20:48:26 +0200

    20250403 Endpoints Insertar, Consultar y Borrar

--- src/Controller/AulasController.php
+++ src/Controller/AulasController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Aulas;
+use App\Entity\Cursos;
+use App\Repository\AulasRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +17,59 @@ final class AulasController extends AbstractController
     {
         return $this->render('aulas/index.html.twig', [
             'controller_name' => 'AulasController',
+        ]);
+    }
+
+    //C3 -> INSERTAR VARIOS REGISTROS (array) POR CÓDIGOS
+    #[Route('/crear-aulas', name: 'app_aulas_insertar_aulas')]
+    public function crearAulas(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        //DEFINIMOS EL ARRAY DE AULAS//
+        $aulas = [
+            "aula1" => [
+                "codigo" => "1.5",
+                "capacidad" => 15,
+                "adaptado" => 1
+            ],
+            "aula2" => [
+                "codigo" => "1.6",
+                "capacidad" => 10,
+                "adaptado" => 0
+            ],
+            "aula3" => [
+                "codigo" => "1.7",
+                "capacidad" => 15,
+                "adaptado" => 1
+            ],
+        ];
+
+        //USO FOREACH PARA RECORRER EL ARRAY, Y METO EN LA TABLA AULA POR AULA
+        foreach ($aulas as $aula) {
+            $nuevaAula = new Aulas();
+            $nuevaAula->setCodigo($aula['codigo']);
+            $nuevaAula->setCapacidad($aula['capacidad']);
+            $nuevaAula->setAdaptado($aula['adaptado']);
+
+            
+            $entityManager->persist($nuevaAula);
+            $entityManager->flush();
+        }
+        return new Response("<h2> Aulas metidas </h2>");
+    }
+
+
+    // R3a -> CONSULTAR POR CLAVE PRINCIPAL
+    // PONGO ESTA RUTA EN EL routes.yaml /mostrar-aula/{id}
+    #[Route('/ver-aulas/{codigo}', name: 'app_aulas_ver_aula')]
+    public function verAula(AulasRepository $repositorio, float $codigo): Response
+    {
+        $aula = $repositorio->find($codigo);
+
+        return $this->render('aulas/aulas.html.twig', [
+            'controller_name' => 'AulasController',
+            'aulas' => [$aula], //<-ESTO ES UN array DE UN SOLO ELEMENTO
         ]);
     }
 }
--- src/Controller/CursosController.php
+++ src/Controller/CursosController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Aulas;
+use App\Entity\Cursos;
+use App\Repository\CursosRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +18,78 @@ final class CursosController extends AbstractController
     {
         return $this->render('cursos/index.html.twig', [
             'controller_name' => 'CursosController',
+        ]);
+    }
+
+    // C4 -> INSERTAR UN REGISTRO POR PARÁMETROS
+    // VARIANTE C2. CONTROLAMOS DATOS TABLA PRINCIPAL 
+    #[Route('/crea-curso/{expediente}/{denominacion}/{codAula}', 
+    name: 'app_cursos_insertar_curso')]
+    public function creaCurso(
+        ManagerRegistry $doctrine,
+        string $expediente,
+        string $denominacion,
+        float $codAula
+    ): Response {
+        $entityManager = $doctrine->getManager();
+
+        $nuevoCurso = new Cursos();
+        $nuevoCurso->setExpediente($expediente);
+        $nuevoCurso->setDenominacion($denominacion);
+
+        //TENGO QUE PONER EL DATO DE LA TABLA PRINCIPAL, USO EL REPOSITORIO DE AUTORES(tabla principal)
+        $aula = $entityManager->getRepository(Aulas::class)->find($codAula);
+
+        //VAMOS A CONTROLAR QUE EL NIF EXISTENTE//
+
+        $mensaje = "";
+
+        if ($aula == null) {
+            $mensaje = "ERROR!! No existe el aula";
+        } else {
+            $nuevoCurso->setCodAula($aula);
+            $entityManager->persist($nuevoCurso);
+            $entityManager->flush();
+            $mensaje = "EXITO!! Se ha introducido el curso.";
+        }
+        return new Response("<h2> $mensaje </h2>");
+    }
+
+    // R2 -> CONSULTAR COMPLETO TABLA RELACIONADA CON BOOTSTRAP
+    // OJO!! MIRAR EL TWIG aticulos.html.twig!!
+    #[Route('/ver-cursos', name: 'app_cursos_ver')]
+    public function verCursos(CursosRepository $repositorio): Response
+    {
+        $cursos = $repositorio->findAll();
+
+        return $this->render('cursos/cursos.html.twig', [
+            'controller_name' => 'CursosController',
+            'cursos' => $cursos, //<-ESTO ES UN arry DE VARIOS ELEMENTOS
+        ]);
+    }
+
+    // D1 ELIMINAR POR ID (TABLA RELACIONADA)
+    #[Route(
+        '/cursos-borrar/{expediente}',
+        name: 'app_cursos_borrar'
+    )]
+    public function borrarCurso(
+        EntityManagerInterface $entityManager,
+        string $expediente
+    ): Response {
+        // BUSCO EL CURSO
+        $repositorioCursos = $entityManager->getRepository(Cursos::class);
+        $curso = $repositorioCursos->find($expediente);
+
+        if ($curso == null) {
+            return new Response("<h1> Curso No encontrado <h1>");
+        } else {
+            $entityManager->remove($curso);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_cursos_ver', [
+            'controller_name' => 'Curso Borrado',
         ]);
     }
 }
